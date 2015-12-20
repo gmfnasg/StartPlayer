01719a5252b5b1ef74c215da704dc7b10c7295ab
gmfnasg <gpx_mark_farng@hotamil.com>
2015-12-20 16:57:27 +0800

    基礎板

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,12 +8,18 @@ public class GameManager : MonoBehaviour {
 	List<string> conditions = new List<string> ();
 	List<List<string>>conditionList = new List<List<string>> ();
 	string[] settings = new string[0];
-	Text conditionText;
+	Text conditionEnText;
+	Text conditionChText;
 	Text cardNumberText;
+	Text logText;
 	void Start () {
-		LoadFile ();
-		conditionText = GameObject.Find ("Condition").GetComponent<Text>();
+		conditionChText = GameObject.Find ("ConditionCh").GetComponent<Text>();
+		conditionEnText = GameObject.Find ("ConditionEn").GetComponent<Text>();
 		cardNumberText = GameObject.Find ("CardNumber").GetComponent<Text>();
+		logText = GameObject.Find ("LogText").GetComponent<Text>();
+
+		LoadFile ();
+
 		Button rndButton = GameObject.Find ("Button").GetComponent<Button> ();
 		rndButton.onClick.AddListener (()=>{
 			RndCondition();
@@ -26,17 +32,51 @@ public class GameManager : MonoBehaviour {
 	}
 	
 	void LoadFile(){
-		string path = "jar:file://" + Application.dataPath + "!/assets" + "/SettingFile.txt";//Application.dataPath + "/SettingFile.txt";
-		//WWW wWA=new WWW(path);  
+		string fileName = "SettingFile.txt";
+		string fileString = "";
 
-		FileStream file = new FileStream (path, FileMode.Open);
-		if (file == null)
-			return;
-		StreamReader sw = new StreamReader(file);
+		#if UNITY_EDITOR
+		string path = Application.dataPath +"/StreamingAssets/"+fileName;
+		StreamReader sw = File.OpenText (path);//new StreamReader(file);
 		if (sw == null)
 			return;
+		fileString = sw.ReadToEnd();
+		ReadFileString(fileString);
+		
+		#elif UNITY_ANDROID
+		StartCoroutine ("LoadFileForAndroid");
+ 
+		#endif
 
-		string [] elementStrs = sw.ReadToEnd().Split('\n');
+
+	}
+
+	void RndCondition(){
+		int rndid = Random.Range (1, conditionList.Count);
+		if(cardNumberText!=null)
+			cardNumberText.text = "編號:" + conditionList[rndid][0];
+		if(conditionChText!=null)
+			conditionChText.text = "條件:\r\n<color=yellow>" + conditionList[rndid][3]+"</color>";
+		if(conditionEnText!=null)
+			conditionEnText.text = "原文:\r\n<color=green>" + conditionList[rndid][2]+"</color>";
+	}
+
+
+	IEnumerator LoadFileForAndroid(){
+		string fileName = "SettingFile.txt";
+		string filePath = "jar:file://" + Application.dataPath + "!/assets/" + fileName;
+		var www = new WWW(filePath);
+		yield return www;
+		if (!string.IsNullOrEmpty(www.error))
+		{
+			DebugLog ("Can't read");
+		}
+		ReadFileString (www.text);
+		RndCondition ();
+	}
+
+	void ReadFileString(string fileString){
+		string[] elementStrs = fileString.Split('\n');
 		int k = 0;
 		string elementStr = elementStrs [k];
 		while (!string.IsNullOrEmpty(elementStr)) {
@@ -48,18 +88,19 @@ public class GameManager : MonoBehaviour {
 				log += " ("+settings[i]+") ";
 				elements.Add(settings[i]);
 			}
-			elementStr = elementStrs [++k];
 			conditionList.Add(elements);
-			Debug.Log (log);
+			
+			if(elementStrs.Length-1>k)
+				elementStr = elementStrs [++k];
+			else
+				elementStr = "";
 		}
-
 	}
 
-	void RndCondition(){
-		int rndid = Random.Range (0, conditionList.Count);
-		if(cardNumberText!=null)
-			cardNumberText.text = "編號:" + conditionList[rndid][0];
-		if(conditionText!=null)
-			conditionText.text = "條件:\r\n<color=yellow>" + conditionList[rndid][3]+"</color>";
+	void DebugLog(string log){
+		if(logText!=null)
+			logText.text += "\r\n"+log;
+		Debug.Log ("\r\n"+log);
 	}
+
 }
